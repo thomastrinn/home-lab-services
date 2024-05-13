cf136ba130ad98f6b27efbe3119d71e97432e38f
thomas <thomas.trinn@gmail.com>
2024-05-13 20:06:28 +0200

    fix deploy-transmission

--- service_deploy.sh
+++ service_deploy.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 export INFISICAL_DISABLE_UPDATE_CHECK=true
-INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$CLIENT_ID --client-secret=$CLIENT_SECRET --silent --plain)
+INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id="$CLIENT_ID" --client-secret="$CLIENT_SECRET" --silent --plain)
 export INFISICAL_TOKEN
 
 infisical run \
