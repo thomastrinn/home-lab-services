ff5285409f8d6ce909f96593db0c9d8f7fc80023
thomas <thomas.trinn@gmail.com>
2024-05-13 20:02:10 +0200

    fix deploy-transmission

---
+++ service_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o pipefail
+
+export INFISICAL_DISABLE_UPDATE_CHECK=true
+INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$CLIENT_ID --client-secret=$CLIENT_SECRET --silent --plain)
+export INFISICAL_TOKEN
+
+infisical run \
+    --project-config-dir=./ \
+    --projectId="$PROJECT_ID" \
+    --path="$SECRET_PATH" \
+    --env=prod \
+    --command='docker compose up -d --pull always --force-recreate'
